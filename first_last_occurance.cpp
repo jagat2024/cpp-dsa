47518e40c1a7596ad05f258773ebf29f02490d06
jagat2024 <jagat324t@gmail.com>
2025-10-13 16:54:45 +0530

    Add files via upload

---
+++ first_last_occurance.cpp
@@ -0,0 +1,57 @@
+//first and last occurancew of element in an array
+#include<bits/stdc++.h>
+using namespace std;
+int firstoccur(vector<int>& arr,int n,int k){
+    int start=0;
+    int end=n-1;
+    int ans=-1;
+    int mid=start+(end-start)/2;
+    while(start<=end){
+        if (arr[mid]==k){
+            ans=mid;
+            end=mid-1;
+        }
+        else if(arr[mid]<k){
+            start=mid+1;
+        }
+        else if(arr[mid]>k){
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+    
+}
+int lastoccur(vector<int>& arr,int n,int k){
+    int start=0;
+    int end=n-1;
+    int ans=-1;
+    int mid=start+(end-start)/2;
+    while(start<=end){
+        if (arr[mid]==k){
+            ans=mid;
+            start=mid+1;
+        }
+        else if(arr[mid]<k){
+            start=mid+1;
+        }
+        else if(arr[mid]>k){
+            end=mid-1;
+        }
+        mid=start+(end-start)/2;
+    }
+    return ans;
+}
+pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
+{
+    pair<int,int>p;
+    p.first=firstoccur(arr,n,k);
+    p.second=lastoccur(arr,n,k);
+    return p;
+}
+int main() {
+    vector<int> arr = {1, 2, 3, 3, 3, 4, 5};
+    int k = 3;
+    auto ans = firstAndLastPosition(arr, arr.size(), k);
+    cout << ans.first << "is the first occurance  index " << ans.second<< "is the last occurance  index"<<endl;
+}
