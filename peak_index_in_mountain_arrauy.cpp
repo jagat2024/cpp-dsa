168bb8052763a2dfa7528ce0a0076ec21eb1ff64
jagat2024 <jagat324t@gmail.com>
2025-10-14 08:15:21 +0530

    Add files via upload

--- peak_index_in_mountain_arrauy.cpp
+++ peak_index_in_mountain_arrauy.cpp
@@ -1,17 +1,27 @@
 #include<bits/stdc++.h>
 using namespace std;
 int peakIndexInMountainArray(vector<int>& arr) {
-        int start=0;
-        int end=arr.size()-1;
-        int mid=start+(end-start)/2;
-        while(start<end){
-            if(arr[mid]<arr[mid+1]){
-                start=mid+1;
-            }
-            else {
-                end=mid;
-            }
-            mid=start+(end-start)/2;
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid;
+
+    while (start < end) {
+        mid = start + (end - start) / 2;
+
+        if (arr[mid] < arr[mid + 1]) {
+            start = mid + 1;
+        } else {
+            end = mid;
         }
-        return start;
+    }
+
+    return start; 
+}
+
+int main(){
+    vector<int> arr={1,3,5,7,6,4,2};
+    int z=peakIndexInMountainArray(arr);
+    cout << "Peak index = " << z << endl;
+    cout << "Peak element = " << arr[z] << endl;
+    return 0;
 }
