a1dcab7981bdecbe01a4d6ecd6f91a1fa9363cde
Jagat Prasanna Shaw <jagat324t@gmail.com>
2025-11-30 19:55:56 +0530

    Create two_sum_problem.cpp

---
+++ problems/two_sum_problem.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+vector<int> twoSum(vector<int>& nums, int target) {
+        int n=nums.size();
+        int i=0;
+        int j=n-1;
+        
+        vector<int> ans;
+        unordered_map<int,int> m;
+        for(int i=0;i<n;i++){
+            int first=nums[i];
+            int second=target-nums[i];
+            if(m.find(second)!=m.end()){
+                ans.push_back(i);
+                ans.push_back(m[second]);
+                break;
+            }
+            m[first]=i;            
+        }
+        return ans;
+    }
+    
