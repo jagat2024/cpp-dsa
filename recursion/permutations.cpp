5dfbd13b05c07f829325ed72226301f3fa27156e
Jagat Prasanna Shaw <jagat324t@gmail.com>
2025-12-09 12:15:53 +0530

    Create permutations.cpp

---
+++ recursion/permutations.cpp
@@ -0,0 +1,48 @@
+/*Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
+
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+Constraints:
+1 <= nums.length <= 6
+-10 <= nums[i] <= 10
+All the integers of nums are unique.*/
+#include<bits/stdc++.h>
+using namespace std;
+void getperms(vector<int> &nums,int idx,vector<vector<int>>&ans){
+    if(idx==nums.size()){
+        ans.push_back(nums);
+        return;
+    }
+    for(int i=idx;i<nums.size();i++){
+        swap(nums[i],nums[idx]);
+        getperms(nums,idx+1,ans);
+        swap(nums[i],nums[idx]);
+    }
+    
+}
+
+vector<vector<int>> permute(vector<int>& nums){
+    vector<vector<int>> ans;
+    getperms(nums,0,ans);
+    return ans;
+}
+int main() {
+    vector<int> nums = {1, 2, 3};
+
+    vector<vector<int>> result = permute(nums);
+
+    // printing result
+    for(auto &v : result) {
+        for(int x : v) {
+            cout << x << " ";
+        }
+        cout << endl;
+    }
+}
