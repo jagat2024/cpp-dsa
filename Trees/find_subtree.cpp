c1e79de9cff737645874e9d4b69b94e0617e1f3e
Jagat Prasanna Shaw <jagat324t@gmail.com>
2026-01-24 12:42:18 +0530

    Implement subtree check for binary trees

---
+++ Trees/find_subtree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool is_identical(TreeNode* p,TreeNode* q){
+        if(p==NULL || q==NULL){
+            return p==q;
+        }
+        return p->val==q-> val && is_identical(p->left,q->left)&& is_identical(p->right,q->right);
+    }
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if(root==NULL || subRoot==NULL){
+            return root==subRoot;
+        }
+        if(root->val ==subRoot->val && is_identical(root,subRoot)){
+            return true;
+        }
+        return isSubtree(root->left,subRoot)||isSubtree(root->right,subRoot);
+    }
+};
