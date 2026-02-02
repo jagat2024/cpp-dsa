39592acf020a4bec5863bb9e2e7e54e8d9cc0bf7
Jagat Prasanna Shaw <jagat324t@gmail.com>
2026-02-02 20:55:40 +0530

    Create differents_path_in_binary_tree.cpp

---
+++ Trees/differents_path_in_binary_tree.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void all_paths(TreeNode* root,string path,vector<string>& ans){
+        if(root->left==NULL && root->right==NULL){
+            ans.push_back(path);
+        }
+        if(root->left){
+            all_paths(root->left,path+"->"+to_string(root->left->val),ans);
+        }
+        if(root->right){
+            all_paths(root->right,path+"->"+to_string(root->right->val),ans);
+        }
+
+    }
+    vector<string> binaryTreePaths(TreeNode* root) {
+        vector<string> ans;
+        string path=to_string(root->val);
+        all_paths(root,path,ans);
+        return ans;
+    }
+};
