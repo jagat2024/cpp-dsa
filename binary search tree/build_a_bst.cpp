665f8c6d6c004f986a417f5a681ce68b0dc9f863
Jagat Prasanna Shaw <jagat324t@gmail.com>
2026-02-19 20:52:50 +0530

    Create build_a_bst.cpp

---
+++ binary search tree/build_a_bst.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+using namespace std;
+class Node{
+    public:
+    int data;
+    Node* left;
+    Node* right;
+    Node(int val){
+        data=val;
+        left=right=NULL;
+    }
+};
+Node* insert(Node* root, int val){
+    if(root==NULL){
+        return new Node(val);
+    }
+    if(root->data>val){
+        root->left=insert(root->left,val);
+    }
+    else{
+        root->right=insert(root->right,val);
+    }
+}
+void inorder(Node* root){
+    if(root==NULL){
+        return;
+    }
+    inorder(root->left);
+    cout<<root->data;
+    inorder(root->right);
+
+}
+Node* buildtree(vector<int> arr){
+    Node* root=NULL;
+    for(auto it: arr){
+        root=insert(root,it);
+    }
+    return root;
+}
+int main(){
+    vector<int> arr={3,2,1,5,4,6};
+    Node* root=buildtree(arr);
+    inorder(root);
+}
